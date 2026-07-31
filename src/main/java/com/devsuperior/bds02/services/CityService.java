9a6b69dc957d8a2ec7479ea8705d2a132e12345f
Allan99 <allanpereira773@gmail.com>
2026-07-31 19:59:30 -0300

    CityControllerIT: findAll and insert

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,37 @@
+package com.devsuperior.bds02.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+
+@Service
+public class CityService {
+	
+	@Autowired
+	private CityRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<CityDTO> findAll(){
+		List<City> cities = repository.findAll(Sort.by("name"));
+		return cities.stream().map(c -> new CityDTO(c)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public CityDTO insert(CityDTO cityDTO) {
+		City city = new City();
+		city.setName(cityDTO.getName());
+		city = repository.save(city);
+		return new CityDTO(city);
+	}
+	
+	
+
+}
