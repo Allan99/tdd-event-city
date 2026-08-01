3528007944aa3aebbf1cbccee440bc576bbdde63
Allan99 <allanpereira773@gmail.com>
2026-08-01 17:53:32 -0300

    EventController created

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -8,6 +8,7 @@ import com.devsuperior.bds02.dto.EventDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.entities.Event;
 import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class EventService {
@@ -17,6 +18,9 @@ public class EventService {
 	
 	@Transactional
 	public EventDTO update(Long id, EventDTO eventDTO) {
+		if(!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Resource no found");
+		}
 		Event event = repository.getReferenceById(id);
 		event.setName(eventDTO.getName());
 		event.setDate(eventDTO.getDate());
